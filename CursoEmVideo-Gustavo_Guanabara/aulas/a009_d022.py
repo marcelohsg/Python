5317549521408622a0518a3b68742a66f791af83
Marcelo Henrique Silva Gomes <110441516+marcelohsg@users.noreply.github.com>
2023-04-11 00:26:33 -0300

    exercícios

--- CursoEmVideo-Gustavo_Guanabara/aulas/a009_d022.py
+++ CursoEmVideo-Gustavo_Guanabara/aulas/a009_d022.py
@@ -2,4 +2,12 @@
 # O nome com todas a letras maiúsculas
 # O nome com todas minúsculas
 # Quantas letras ao todo(sem considerar espaços)
-# Quantas letras tem o primeiro nome 
+# Quantas letras tem o primeiro nome
+nome = str(input('Digite seu nome completo: ')).strip()
+print(f'Seu nome em maiúsculas é {nome.upper()}')
+print(f'Seu nome em minúscilas é {nome.lower()}')
+n1 = len(nome)
+n2 = nome.count(' ')
+nome2 = n1-n2
+print(f'Seu nome tem ao todo {nome2} letras')
+print(f'Seu primeiro nome tem {len(nome.split()[0])} letras')
--- CursoEmVideo-Gustavo_Guanabara/aulas/a009_d023.py
+++ CursoEmVideo-Gustavo_Guanabara/aulas/a009_d023.py
@@ -3,4 +3,14 @@
 # unidade = 4
 # dezena = 3
 # centena = 8
-# milhar = 1  
+# milhar = 1
+num = int(input('Digite um número de 0 a 9999: '))
+u = num % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print(f'Analisando o número {num}')
+print(f'Unidade = {u}') 
+print(f'Dezena = {d}')
+print(f'Centena = {c}')
+print(f'Milhar = {m}')   
