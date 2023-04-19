05e1bdbd422e9db88503c6a9a60f40f36e5b6269
Marcelo Henrique Silva Gomes <110441516+marcelohsg@users.noreply.github.com>
2023-04-19 11:54:37 -0300

    resolução de exercícios

--- CursoEmVideo-Gustavo_Guanabara/aulas/a014_d057.py
+++ CursoEmVideo-Gustavo_Guanabara/aulas/a014_d057.py
@@ -1,1 +1,9 @@
 # Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto.
+sexo = ''
+while sexo != 'M' and sexo != 'F':
+    sexo = input('Informe seu sexo [M/F]: ').upper().strip()
+    if sexo != 'M' and sexo != 'F':
+        print('Opção inválida. Digite "M" para Masculino ou "F" para Feminino!')
+    else:
+        print(f'Sexo {sexo} registrado com sucesso!')
+
--- CursoEmVideo-Gustavo_Guanabara/aulas/a014_d058.py
+++ CursoEmVideo-Gustavo_Guanabara/aulas/a014_d058.py
@@ -1,1 +1,24 @@
-# Melhore o jogo do desafio 028 onde o computador vai 'pensar' em um número entre 0 e 10. Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer. 
+# Melhore o jogo do desafio 028 onde o computador vai 'pensar' em um número entre 0 e 10. Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer.
+from random import randint
+print('-=-' * 20)
+print('Vou pensar em um número entre 0 e 10. Tente adivinhar...')
+print('-=-' * 20)
+x = randint(1, 10)
+cont = 0
+'''if a == x:
+    print(f'O número escolhido foi {x}. VOCÊ GANHOU!')
+else:
+    print(f'O número escolhido foi {x}. VOCÊ PERDEU!')
+print('== FIM ==')'''
+a = ''
+while a != x:
+    a = int(input('Em qual número eu pensei? '))
+    if a != x:
+        cont += 1
+        print('VOCÊ PERDEU!')
+        if a < x:
+            print('Mais... Tente outra vez!')
+        else:
+            print('Menos... Tente outra vez!')
+print('VOCÊ GANHOU!')
+print(f'Você tentou {cont} vezes até acertar.')
