05ffe5d4c0810078394f89c4517eb0816240c8fd
Marcelo Henrique Silva Gomes <110441516+marcelohsg@users.noreply.github.com>
2023-04-25 11:39:05 -0300

    Update a015_d069.py

--- CursoEmVideo-Gustavo_Guanabara/aulas/a015_d069.py
+++ CursoEmVideo-Gustavo_Guanabara/aulas/a015_d069.py
@@ -5,3 +5,29 @@ A) quantas pessoas tem mais de 18 anos.
 B) quantos homens foram cadastrados.
 
 C) quantas mulheres tem menos de 20 anos.'''
+contIdade = contHomem = contMulher = 0
+while True:
+    print('=-'*15)
+    print('CADASTRE UMA PESSOA')
+    print('=-'*15)
+    idade = int(input('Idade: '))
+    sexo = continuar = ' '
+    while sexo not in 'MF':        
+        sexo = input('Sexo [M/F]: ').upper().strip()[0]        
+    while continuar not in 'SN':
+        print('=-'*15)
+        continuar = input('Você quer continuar [S/N]? ').upper().strip()[0]  
+    if idade > 18:
+        contIdade += 1
+    if sexo == 'M':
+        contHomem += 1
+    if sexo == 'F':
+        if idade < 20:
+            contMulher += 1
+    if continuar == 'N':
+        print('Fim programa!')
+        print('=-'*15)
+        break
+print(f'A) Pessoas com mais de 18 anos: {contIdade}')
+print(f'B) Homens cadastrados: {contHomem}')
+print(f'C) Mulheres com menos de 20 anos: {contMulher}')
