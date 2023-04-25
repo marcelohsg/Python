e3db8f0585f7b50f5dc15ed293d114b8b59ee748
Marcelo Henrique Silva Gomes <110441516+marcelohsg@users.noreply.github.com>
2023-04-25 17:56:55 -0300

    Create a016_tuplas.py

---
+++ CursoEmVideo-Gustavo_Guanabara/aulas/a016_tuplas.py
@@ -0,0 +1,10 @@
+''' 
+Variáveis compostas = [tuplas, listas, dicionários]
+-> As tuplas são imutáveis = Assim que você define uma tupla, ela irá permanecer assim até o fim do programa.
+-> Toda tupla começa com ().
+-> Pode ter dados de tipos diferentes dentro das tuplas
+'''
+lanche = ('hamburgue', 'Suco', 'Pizza', 'Pudim')
+for comida in lanche:
+    print(f'Eu vou comer {comida}')
+print('Comir pra caramba!')
