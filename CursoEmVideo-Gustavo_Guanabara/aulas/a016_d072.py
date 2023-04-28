939901235eb70e56887e9da1187304d55e6fc872
Marcelo Henrique Silva Gomes <110441516+marcelohsg@users.noreply.github.com>
2023-04-28 10:37:36 -0300

    Update a016_d072.py

---
+++ CursoEmVideo-Gustavo_Guanabara/aulas/a016_d072.py
@@ -0,0 +1,14 @@
+# Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+nome = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze',
+        'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+while True:
+    while True:
+        num = int(input('Digite um número entre 0 e 20: '))
+        if 0 <= num <= 20:
+            break
+        print('Tente novamente. ', end='')
+    print(f'Você digitou o número {nome[num]}.')
+    escolha = input('Você deseja continuar [S/N]? ').strip().upper()[0]
+    if escolha == 'N':
+        print('FIM DO PROGRAMA!')
+        break
