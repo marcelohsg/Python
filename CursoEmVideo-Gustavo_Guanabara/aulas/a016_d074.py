221f8f527358a3c90aa754a518507908f0960498
Marcelo Henrique Silva Gomes <110441516+marcelohsg@users.noreply.github.com>
2023-04-29 21:45:20 -0300

    Update a016_d074.py

--- CursoEmVideo-Gustavo_Guanabara/aulas/a016_d074.py
+++ CursoEmVideo-Gustavo_Guanabara/aulas/a016_d074.py
@@ -1,18 +1,33 @@
 # Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla. Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla.
 from random import randint
-cont = n1 = n2 = n3 = n4 = n5 = 0
+'''FORMA TRADICIONAL!
+cont = n1 = n2 = n3 = n4 = n5 = maior = menor = 0
 while cont <= 5:
     computador = randint(1,20)
     if cont == 1:
         n1 = computador
-    elif cont == 2:
+        maior = menor = computador
+    else:
+        if computador > maior:
+            maior = computador
+        if computador < menor:
+            menor = computador        
+    if cont == 2:
         n2 = computador
-    elif cont == 3:
+    if cont == 3:
         n3 = computador
-    elif cont == 4:
+    if cont == 4:
         n4 = computador
-    elif cont == 5:
-        n5 = computador
+    if cont == 5:
+        n5 = computador   
     cont += 1
 listagem = (n1, n2, n3, n4, n5)
-
+print(f'Números gerados: {listagem}')
+print(f'O MAIOR número gerado foi {maior}.')
+print(f'O MENOR número gerado foi {menor}.')'''
+num = (randint(1,20), randint(1,20), randint(1,20), randint(1,20), randint(1,20))
+print('Os valores soteados foram: ', end='')
+for n in num:
+    print(f'{n} ', end='')
+print(f'\nO maior número sorteado foi {max(num)}.')
+print(f'O menor número sorteado foi {min(num)}.')
