f8a1b62f9b86599d963168a5c7374b3ae59cb480
Marcelo Henrique Silva Gomes <110441516+marcelohsg@users.noreply.github.com>
2023-05-03 09:21:18 -0300

    Update a017_d079.py

--- CursoEmVideo-Gustavo_Guanabara/aulas/a017_d079.py
+++ CursoEmVideo-Gustavo_Guanabara/aulas/a017_d079.py
@@ -1,1 +1,15 @@
 # Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. Caso o número já exista lá dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados, em ordem crescente.
+valores = []
+while True:
+    n = int(input('Digite um valor: '))
+    if n not in valores:
+        valores.append(n)
+        print('Valor adicionado com sucesso!')
+    else:
+        print('Valor duplicado! Não vou adicionar...')
+    desejo = input('Você deseja continuar [S/N}? ').upper().strip()[0]
+    if desejo == 'N':
+        break
+valores.sort()
+print(f'Os valores digitados foram: {valores}')
+
