957be8fe6baebcc881654bc4c6791fca04bc0aa5
Marcelo Henrique Silva Gomes <110441516+marcelohsg@users.noreply.github.com>
2023-05-04 11:36:15 -0300

    Update a017_d083.py

--- CursoEmVideo-Gustavo_Guanabara/aulas/a017_d083.py
+++ CursoEmVideo-Gustavo_Guanabara/aulas/a017_d083.py
@@ -1,1 +1,17 @@
 # Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
+exp = str(input('Digite a  expressão: '))
+lista = []
+for item in exp:
+    if item == '(':
+        lista.append('(')
+    elif item == ')':
+        if len(lista) > 0:
+            lista.pop()
+        else:
+            lista.append(')')
+            break
+if len(lista) == 0:
+    print('Sua expressão está correta!')
+else:
+    print('Sua expressão está errada!')
+    
