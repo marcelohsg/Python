172ff64780b3681632a78fa727f8072b4f67f6f6
Marcelo Henrique Silva Gomes <110441516+marcelohsg@users.noreply.github.com>
2023-05-05 18:25:07 -0300

    Update a018_d085.py

---
+++ CursoEmVideo-Gustavo_Guanabara/aulas/a018_d085.py
@@ -0,0 +1,12 @@
+# Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
+lista = [[], []]
+for cont in range(0, 7):
+    num = int(input(f'Digite o {cont+1}º valor: '))
+    if num % 2 == 0:
+        lista[0].append(num)
+    else:
+        lista[1].append(num)
+lista[0].sort()
+lista[1].sort()
+print(f'Os valores pares digitados foram: {lista[0]}')
+print(f'Os valores ímpares digitados foram: {lista[1]}')
