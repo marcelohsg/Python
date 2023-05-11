08bfde8df6530e3a8b1ae457566c05d325343ba9
Marcelo Henrique Silva Gomes <110441516+marcelohsg@users.noreply.github.com>
2023-05-11 09:41:52 -0300

    exercício

--- CursoEmVideo-Gustavo_Guanabara/aulas/a019_d090.py
+++ CursoEmVideo-Gustavo_Guanabara/aulas/a019_d090.py
@@ -1,1 +1,13 @@
 # Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário. No final, mostre o conteúdo da estrutura na tela.
+aluno = {}
+aluno['nome'] = input('Nome: ')
+aluno['media'] = float(input(f'Média de {aluno["nome"]}: '))
+print('-='*30)
+print(f'- O nome é igual a {aluno["nome"]}')
+print(f'- Média é igual a {aluno["media"]}')
+if aluno['media'] < 7:
+    print('- Situação é igual a recuperação!')
+elif aluno['media'] < 4:
+    print('- Situação é igual a reprovado!')
+else:
+    print('- Situação é igual a aprovado!')
--- CursoEmVideo-Gustavo_Guanabara/aulas/a019_dicionarios02.py
+++ CursoEmVideo-Gustavo_Guanabara/aulas/a019_dicionarios02.py
@@ -14,4 +14,6 @@ for c in range(0,3):
     estado['uf'] = input('Unidade Federativa: ')
     estado['sigla'] = input('Sigla do Estado: ')
     brazil.append(estado.copy())
-print(brazil)
+for e in brasil:
+    for k, v in e.items():
+        print(f'O campo {k} tem valor {v}.')
