3d186ac2f56390d4134c017bedd51b1c58dd7e23
Marcelo Henrique Silva Gomes <110441516+marcelohsg@users.noreply.github.com>
2023-05-13 22:58:39 -0300

    Update a019_d095.py

--- CursoEmVideo-Gustavo_Guanabara/aulas/a019_d095.py
+++ CursoEmVideo-Gustavo_Guanabara/aulas/a019_d095.py
@@ -1,1 +1,14 @@
-
+# Aprimore o desafio 93 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.
+jogador = {}
+jogador['nome'] = str(input('Nome: '))
+jogador['jogos'] = int(input('Quantidade de jogos: '))
+if jogador['jogos'] != 0:
+    totg = 0
+    for cont in range (1, jogador['jogos']+1):
+        gols = int(input(f'  Gol(s) marcado(s) na {cont}º partida: '))
+        totg += gols  
+jogador['Total de gols'] = totg
+print('-='*30)
+print(jogador)
+for k, v in jogador.items():
+    print(f'{k}: {v}')
