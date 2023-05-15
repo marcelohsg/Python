dda0d8c2049858e53843b629fb4494ec4fc53b5a
Marcelo Henrique Silva Gomes <110441516+marcelohsg@users.noreply.github.com>
2023-05-15 17:47:56 -0300

    Funções

---
+++ CursoEmVideo-Gustavo_Guanabara/aulas/a020_funções01.py
@@ -0,0 +1,9 @@
+# FUNÇÕES
+def titulo(txt):
+    print('-'*30)
+    print(txt)
+    print('-'*30)
+
+
+titulo('CURSO EM VÍDEO')
+titulo('PAYTHON')
---
+++ CursoEmVideo-Gustavo_Guanabara/aulas/a020_funções02.py
@@ -0,0 +1,8 @@
+def soma(a, b):
+    print(f'{a} + {b} = {a+b}')
+
+# PROGRAMA PRINCIPAL
+soma(5, 5)
+soma(7, 2)
+soma(5, 3)
+soma(13, 15.7)
