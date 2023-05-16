c1759d87a232878b69b3a5c70ffb56b260ad3d4e
Marcelo Henrique Silva Gomes <110441516+marcelohsg@users.noreply.github.com>
2023-05-16 17:02:32 -0300

    Update a020_d097.py

--- CursoEmVideo-Gustavo_Guanabara/aulas/a020_d097.py
+++ CursoEmVideo-Gustavo_Guanabara/aulas/a020_d097.py
@@ -1,7 +1,14 @@
 '''Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável. 
 Ex: 
-escreva(‘Olá, Mundo!’) Saída:
+escreva("Olá, Mundo!") Saída:
 ~~~~~~~~~~~~
 Olá, mundo!
 ~~~~~~~~~~~~
 '''
+def escreva(txt):
+    print('~'*(len(txt)+4))
+    print(f'  {txt}')
+    print('~'*(len(txt)+4))
+
+texto = str(input('Digite um texto: '))
+escreva(texto)
