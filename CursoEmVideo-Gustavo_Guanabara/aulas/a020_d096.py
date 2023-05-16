83f81cc0d02e3a846f758582630699dad19d8e56
Marcelo Henrique Silva Gomes <110441516+marcelohsg@users.noreply.github.com>
2023-05-16 15:52:34 -0300

    exer. funções

--- CursoEmVideo-Gustavo_Guanabara/aulas/a020_d096.py
+++ CursoEmVideo-Gustavo_Guanabara/aulas/a020_d096.py
@@ -1,8 +1,10 @@
 # Faça um programa que tenha uma função chamada área(), que receba as dimensões de um terreno retangular (largura e comprimento) e mostre a área do terreno.
 def area(a, b):
-    print(f'A área do terreno é {a*b:.2f}m²')
+    print(f'A área de um terreno {a:.0f}x{b:.0f} é de {a*b:.2f}m².')
 
-
-largura = float(input('Qual a largura do terreno? '))
-comprimento = float(input('Qual o comprimento do terreno? '))
+print('=-'*15)
+print('CONTROLE DE TERRENO')
+print('=-'*15)
+largura = float(input('Qual a largura do terreno (m)? '))
+comprimento = float(input('Qual o comprimento do terreno (m)? '))
 area(largura, comprimento)
---
+++ CursoEmVideo-Gustavo_Guanabara/aulas/a020_d097.py
@@ -0,0 +1,7 @@
+'''Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável. 
+Ex: 
+escreva(‘Olá, Mundo!’) Saída:
+~~~~~~~~~~~~
+Olá, mundo!
+~~~~~~~~~~~~
+'''
